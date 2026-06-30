386a828d17e1aeab9c0cc78d679de3e0bdfeda3f
seghobs <seghobs@example.com>
2026-06-30 23:00:35 +0300

    config: change Flask server port to 3434 in flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -35,7 +35,7 @@ start_automation()
 
 if __name__ == "__main__":
     host = "127.0.0.1"
-    port = 5000
+    port = 3434
 
     flask.cli.show_server_banner = lambda *args, **kwargs: None
     logging.getLogger("werkzeug").setLevel(logging.ERROR)
